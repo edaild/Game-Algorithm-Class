84d16ea35139962997431434b646e7761a7c099d
edaild <ch5183sb@gmail.com>
2025-09-24 15:59:53 +0900

    [Feat] 과제 미완료

---
+++ Assets/Scripts/20250924 4주차 수업/IventoryBinzry.cs
@@ -0,0 +1,58 @@
+using JetBrains.Annotations;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class IventoryBinzry : MonoBehaviour
+{
+    public List<Item> items = new List<Item>();
+
+     void Start()
+    {
+        items.Add(new Item("Potion", 5));
+        items.Add(new Item("High_Potion", 2));
+        items.Add(new Item("Elixir", 1));
+        items.Add(new Item("Sword", 1));
+
+        // 탐색 전 정렬 (이름 기준)
+        items.Sort((a, b) => a.itemName.CompareTo(b.itemName));
+
+        // 탐색 테스트
+        Item found = FindItem("Elixir");
+        if(found != null)
+        {
+            Debug.Log($"[이진 탐색] 찾는 아이템: {found.itemName}, 개수 : {found.quantity}");
+        }
+        else
+        {
+            Debug.Log("[이진 탐색] 아이템을 찾을 수 없습니다.");
+        }
+    }
+
+    public Item FindItem(string targetName)
+    {
+        int left = 0;
+        int right = items.Count - 1;
+        
+        while(left <= right)
+        {
+            int mid = (left + right) / 2;
+            int compare = items[mid].itemName.CompareTo(targetName);
+
+            if(compare == 0)
+            {
+                return items[mid];      // 찾음
+            }
+            else if(compare < 0)
+            {
+                left = mid + 1;     // 오른쪽탐색
+            }
+            else
+            {
+                right = mid - 1;    // 왼쪽 탐색
+            }
+        }
+        return null;    // 못 찾음
+    }
+}
---
+++ Assets/Scripts/20250924 4주차 수업/Search.cs
@@ -0,0 +1,139 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using UnityEngine;
+using TMPro;
+using Unity.Burst.Intrinsics;
+using UnityEngine.Rendering;
+
+public class Search : MonoBehaviour
+{
+    [Header("UI Refurerences (TMP)")]
+    public TMP_InputField inputItemCount;
+    public TMP_InputField inputSearchCount;
+    public TMP_Text resultText;
+
+    private List<Item> items = new List<Item>();
+
+    private long sortSteps;         // 정렬 비교 횟수
+
+    private long linearSteps;       // 선형 탐색 비교 횟수
+
+    private long binarySteps;       // 이진 탐색 비교 횟수
+
+   public void OnFindButton()
+    {
+        if (!int.TryParse(inputItemCount.text, out int itemCount)) itemCount = 10000;
+        if (!int.TryParse(inputSearchCount.text, out int searchCount)) searchCount = 100;
+
+        // 아이템 초기화
+        items.Clear();
+
+        for(int i = 0; i < itemCount; i++)
+        {
+            items.Add(new Item($"Item_{Random.Range(0, itemCount):D5}", 1));
+        }
+
+        // 탐색 대상 생성
+        List<string> targets = new List<string>();
+        for (int i = 0; i < searchCount; i++)
+        {
+            targets.Add($"Item_{Random.Range(0, itemCount):D5}");
+        }
+        // 1. 선형 탐색
+        linearSteps = 0;
+        foreach(var t in targets)
+        {
+           linearSteps += FindItemLinearSteps(t);
+        }
+
+        // 2. 퀵소트 + 이진 탐색
+
+        sortSteps = 0;
+        QuickSort(items, 0, items.Count - 1); // 정렬 (비교 횟수 기록됨)
+
+        binarySteps = 0;
+        foreach (var t in targets)
+        {
+            binarySteps += FindItemBinarySteps(t);
+        }
+
+        // 결과 출력
+        resultText.text =
+            $"Item Count : {itemCount}\n" +
+            $"Search Count : {searchCount}\n\n\n" +
+            $"Linear Search Total Comparisons: {linearSteps} \n\n" +
+            $"Quick Sort Compaarisons : {sortSteps} \n" +
+            $"binary Sarch Total Comparisons: {binarySteps} \n" +
+            $"Total (Sort + Binary) : {sortSteps + binarySteps}";
+
+
+    }
+
+    private void QuickSort(List<Item> list, int left, int right)
+    {
+        if (left < right) return;
+        
+            int pivotIndex = Partition(list, left, right);
+            QuickSort(list, left, pivotIndex - 1);               // 피벗 왼쪽 정렬
+            QuickSort(list , pivotIndex + 1, right);              // 피벗 오른쪽 정렬
+       
+    }
+
+    private int Partition(List<Item> list, int left, int right)
+    {
+        Item pivot = list[right];
+        int i = left - 1;
+
+        for(int j = left; j < right; j++)
+        {
+            sortSteps++;        // 비교 1회
+            if(list[j].itemName.CompareTo(pivot.itemName) <= 0)
+            {
+                i++;
+                Swap(list, i, j);
+            }
+        }
+        Swap(list, i + 1, right);
+        return i + 1;
+    }
+
+    private void Swap(List<Item> list,int a, int b)
+    {
+        Item temp = list[a];
+        list[a] = list[b];
+        list[b] = temp;
+    }
+
+    private int FindItemLinearSteps(string target)
+    {
+        int steps = 0;
+        foreach(Item item in items)
+        {
+            steps++;
+            if (item.itemName == target)
+                return steps;
+        }
+        return steps;
+    }
+
+    private int FindItemBinarySteps(string target)
+    {
+        int steps = 0;
+        int left = 0, right = items.Count - 1;
+
+        while (left <= right)
+        {
+            steps++;
+            int mid = (left + right) / 2;
+            int cmp = items[mid].itemName.CompareTo(target);
+
+            if (cmp == 0) return steps;
+            else if (cmp < 0) left = mid + 1;
+            else right = mid - 1;
+        }
+        return steps;
+    }
+
+
+}
